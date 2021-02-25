4655a466c38084b641fd02ee1c7f1e8b03b7c3bb
buddy@dutchmedialab.nl <buddy@dutchmedialab.nl>
2021-02-25 13:42:18 +0100

    SK-25: Require @standardkit, add buttons and apply a theme.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@standardkit/angular-components';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
